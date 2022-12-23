e13e173dbddf32d7e8c4908d6e05b9d9be2b35df
jitain1 <singhjitainj@gmail.com>
2022-12-23 18:27:01 +0530

    Employee Wage Calculation Added

--- src/com/bridgelabz/EmployeeWage.java
+++ src/com/bridgelabz/EmployeeWage.java
@@ -2,15 +2,23 @@ package com.bridgelabz;
 
 public class EmployeeWage {
 
-		static final int IS_FULL_TIME = 1; 
-		
-		public static void main(String[] args) {
-			System.out.println("Welcome to employee wage calculation problem.");
-			
-			int empCheck = (int) (Math.random()*2);
-			if(IS_FULL_TIME == empCheck) {
-				System.out.println("Employee is Present");
-			}else
-				System.out.println("Employee is Absent.");		
+	static final int WAGE_PER_HOUR = 20;
+	static final int IS_FULL_TIME = 1;
+	static final int FULL_DAY_HOUR = 8; 
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to employee wage calculation problem.");
+
+		int dailyWage = 0;
+		int empCheck = (int) (Math.random()*2);
+		if(IS_FULL_TIME == empCheck) {
+			System.out.println("Employee is Present");
+			dailyWage = WAGE_PER_HOUR * FULL_DAY_HOUR;
+			System.out.println("Wage of the employee is: " + dailyWage);
+		}else {
+			System.out.println("Employee is absent.");
+			System.out.println("Wage of the employee is: " + dailyWage);
 		}
+
 	}
+}
